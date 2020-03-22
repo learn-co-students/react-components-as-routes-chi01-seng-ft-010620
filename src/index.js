6805fd3a5d2b19a6bbc3dfb2d5bb2af36667b8e9
Cesar <cesarmota@ymail.com>
2020-03-22 18:26:21 -0500

    Done.

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+import React from "react";
+import ReactDOM from "react-dom";
+import { BrowserRouter as Router, Route } from "react-router-dom";
 
 const Home = () => {
   return (
@@ -8,8 +9,41 @@ const Home = () => {
     </div>
   );
 };
+const About = () => {
+  return (
+    <React.Fragment>
+      <h1>This is my about component!</h1>
+    </React.Fragment>
+  );
+};
+const Login = () => {
+  // <React.Fragment>
+   return(<div>
+    <form>
+      <div>
+        <input type="text" name="username" placeholder="Enter username" />
+        <label htmlFor="username">Username</label>
+      </div>
+      <div>
+        <input type="text" name="password" placeholder="Enter password"/>
+        <label htmlFor="password">Password</label>
+      </div>
+      <input type="submit" value="Login"/>
+    </form>
+    <h1>This is my login</h1>
+  </div>
+  ) 
+  {/* </React.Fragment>; */}
+};
 
 ReactDOM.render(
-  <Home />,
-  document.getElementById('root')
+  <Router>
+        <div>
+    <Route exact path="/" component={Home} />
+      
+    <Route path="/about" component={About}/>
+    <Route path="/login" component={Login}/>
+    </div>
+  </Router>,
+  document.getElementById("root")
 );
